b4ef7c1d43ec5fdf03a6d7ee808427b4df90ba7c
tanakablog <Tanakancolle@gmail.com>
2016-12-14 19:04:17 +0900

    アセットバンドル管理クラス作成

---
+++ Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    /// <summary>
+    /// MonoBehaviour用シングルトンクラス
+    /// </summary>
+    public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour {
+
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        private static T _instance;
+        public static T Instance {
+            get { 
+                // インスタンスチェック
+                if (_instance == null) {
+                    // インスタンス検索
+                    _instance = FindObjectOfType<T> ();
+                    if (_instance == null) {
+                        // インスタンス生成
+                        var type = typeof(T);
+                        var obj = new GameObject (type.ToString () + "(Singleton)");
+                        _instance = obj.AddComponent<T> ();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
